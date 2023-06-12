fd90b62deda36baf15d07e7beef1b0b4dfab82f7
Rnathunge G.S.G. (IT210437906) <it21043796@my.sliit.lk>
2023-06-12 19:28:37 +0530

    nav bar completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import Header from './Components/Header';
+import addUser from './Components/addUser';
+import {BrowserRouter as Router,Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Santhush Gimhana</h1>
-      <Header/>
-    </div>
+    <Route>
+      <div className="App">
+        <Header/>
+          <Route path="/add" exact componenet ={addUser}/>
+        <addUser/>
+      </div>
+    </Route>
   );
 }
 
---
+++ frontend/src/Components/addUser.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class addUser extends React.Component{
+    render(){
+        return(
+            <div>
+                Santhush is me
+            </div>
+        )
+    }
+}
+
+export default addUser;
