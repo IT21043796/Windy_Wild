7c30eaa59b4440a1079e37ecbc7dde438d4504f6
Rnathunge G.S.G. (IT210437906) <it21043796@my.sliit.lk>
2023-06-12 16:14:57 +0530

    create update view done

---
+++ backend/routes/users.js
@@ -0,0 +1,55 @@
+const router = requir("express").Router();
+let User = require ("../models/user.js");
+
+//create.........
+
+router.route("/add").post((req,res)=>{
+
+    const name = req.body.name;
+    const age = req.body.age;
+    const address = req.body.address;
+
+    const newUser = new User({
+        name,
+        age,
+        address
+    }) 
+    
+    newUser.save().then(()=>{
+        res.json("User Added")
+    }).catch((err)=>{
+        console.log(err); 
+    })
+})
+
+router.route("/").get((req,res)=>{
+
+    User.find().then((users)=>{
+        res.json(users)
+    }).catch((err)=>{
+        console.log(err)
+    })
+
+})
+
+//update
+
+router.route("/update/:id").put(async (req,res)=>{
+    let userId = req.params.id;
+    const {name, age, address} = req.body;
+
+    const userUpdate = {
+        name,
+        age,
+        address
+    }
+
+    const update = await User.findByIdAndUpdate(userId,userUpdate)
+
+    res.status(200).send({status : "User updated",user:update})
+
+})
+
+
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const cors = require("cors")
+const bodyparser = require("body-parser")
+
+const app = express()
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8070
+
+app.use(cors());
+app.use(bodyparser.json());
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+    //useCreateIndex: true,
+    useNewUrlParser : true,
+    //useUnifiedTopologyL : true,
+    //useFindAndModify : false
+})
+
+const connection = mongoose.connection;
+connection.once("open",() =>{
+    console.log("Mongodb Connection Successfull");
+})
+
+const userRouter = require("./routes/users.js");
+
+app.use("/user",userRouter)
+
+app.listen(PORT, ()=>{
+    console.log(`Server is up and running in port ${PORT}`)
+})
