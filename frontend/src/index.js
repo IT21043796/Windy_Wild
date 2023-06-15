b2f9f331cf301130ec9dd0f0b8a52c3a8950afbf
Rnathunge G.S.G. (IT210437906) <it21043796@my.sliit.lk>
2023-06-15 13:01:56 +0530

    material icons installed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import CloseIcon from '@mui/icons-material/Close';
+
 
 ReactDOM.render(
   <React.StrictMode>
